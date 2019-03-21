bedd28f39377dca0300628bdb3e3d46607ed430d
bettmensch88 <sebastian.scherer@yahoo.de>
2019-03-21 21:46:41 +0000

    basic walker class done

--- lib/game_class.py
+++ lib/game_class.py
@@ -28,16 +28,51 @@ class Walker(Sprite):
         
         # set positional and orientational attributes
         self.x = vec(pos_x,pos_y)
-        self.v = vec(0,SPEED).rotate(theta)
+        self.v = vec(0,D_X).rotate(theta)
+        self.theta = theta
         
         # set image and rect attributes
-        self.image = pg.transform.rotate(image_surface,theta)
-        self.rect = image_surface.get_rect()
+        self.original_image = image_surface
         
+        # initialize position attributes by first call to update()
+        self.update(turn = NONE)
 
-
+    def update(self,
+               turn):
+        
+        # sanity check commands
+        assert turn in (LEFT,NONE,RIGHT)
+        
+        # update angle according to steer
+        if turn == LEFT:
+            d_theta = D_THETA
+        elif turn == RIGHT:
+            d_theta = -D_THETA
+        elif turn == NONE:
+            d_theta = 0
+            
+        # update angle and velocity according to steer
+        self.theta += d_theta
+        self.v = self.v.rotate(d_theta)
+        
+        # get new position with updated velocity
+        self.x += self.v
+        self.rect.center = (int(self.x.x, int(self.x.y)))
+        
+        # update image surface orientation as needed
+        if turn != NONE:
+            self.image = pg.transform.rotate(self.original_image,
+                                             self.theta)
+            self.rect = self.image.get_rect()
+        
 class Walk_With_AI(object):
     
     def __init__(self,
                  frames_per_second,
-                 window_size = (400,400))
+                 window_size = WINDOW_SIZE):
+        
+        # initialize pygame window
+        pg.init()
+        
+        # get game clock
+        self.clock = pg.time.Clock()
--- lib/settings.py
+++ lib/settings.py
@@ -5,5 +5,19 @@ Created on Thu Mar 21 21:20:44 2019
 @author: bettmensch
 """
 
-# speed of walker in pixel/second
-SPEED = 10
+# size of game window in pixel
+WINDOW_SIZE = (400,400)
+
+# frames per second
+FRAMES_PER_SECOND = 10
+
+# speed of walker in pixel/ frame
+D_X = 10
+
+# angular speed in degree / frame
+D_THETA = 0.5
+
+# directional instructions for walker
+LEFT = 'LEFT'
+NONE = 'NONE'
+RIGHT = 'RIGHT'

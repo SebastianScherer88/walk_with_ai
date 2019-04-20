3230787f5f4af010079afba9f8c6ccca4cef977f
bettmensch88 <sebastian.scherer@yahoo.de>
2019-04-20 19:08:46 +0100

    added loading and building function to game generic util script; started updating the pong main accordingly

--- lib/pong_game/pong_main.py
+++ lib/pong_game/pong_main.py
@@ -16,79 +16,22 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir) # lib dir of current repo: contains the game classes
 deep_learning_dir = os.path.join(os.path.dirname(os.path.dirname(parentdir)),"deep_learning_library") # dir in dl repo: contains dl classes
 
-sys.path.append(deep_learning_dir)
+# game generic util functions
+sys.path.append(parentdir)
+from util import create_and_prep_net, load_oldest_model
 
+# diy deep learning lib from other repo
+sys.path.append(deep_learning_dir)
 from diy_deep_learning_library import FFNetwork, PG
 
 
 # --- main
 def main():
     
-    # --- build conv net
-    # build network
-    neural_net = FFNetwork(2)
-    
-    kernelSize1 = 10
-    channels1 = 2
-    stride1 = 5
-    padding1 = 'valid'
-    
-    poolingSize2 = 5
-    stride2 = 2
-    padding2 = 'valid'
-    
-    kernelSize3 = 5
-    channels3 = 4
-    stride3 = 1
-    padding3 = 'valid'
-    
-    poolingSize4 = 2
-    stride4 = 2
-    padding4 = 'valid'
-    
-    n4 = 30
-    
-    n_output = 3
-    
-    neural_net.addConvLayer(kernelHeight=kernelSize1,
-                       kernelWidth=kernelSize1,
-                       channels=channels1,
-                       stride=stride1,
-                       padding=padding1,
-                       activation='tanh')
-    neural_net.addPoolingLayer(poolingHeight=poolingSize2,
-                           poolingWidth=poolingSize2,
-                           stride=stride2,
-                           padding=padding2,
-                           poolingType='max')
-    neural_net.addConvLayer(kernelHeight=kernelSize3,
-                           kernelWidth=kernelSize3,
-                           channels=channels3,
-                           stride=stride3,
-                           padding=padding3,
-                           activation='tanh')
-    neural_net.addPoolingLayer(poolingHeight=poolingSize4,
-                           poolingWidth=poolingSize4,
-                           stride=stride4,
-                           padding=padding4,
-                           poolingType='max')
-    
-    neural_net.addFlattenConvLayer()
-    
-    neural_net.addFCLayer(n4,activation='tanh')
-    
-    neural_net.addFCLayer(n_output,activation='softmax')
-    
-    neural_net.fixateNetwork(np.zeros((10,N_CHANNELS,WINDOW_SIZE[0],WINDOW_SIZE[1])))
-    
-    print(neural_net)
-    
-    # prep network for inference without training it
-    neural_net.oneHotY(np.array([UP,DOWN,NONE]))
-
-    neural_net.initialize_layer_optimizers('sgd',eta = 0.001,gamma = 0.99,epsilon = 0.00000001,lamda = 0,batchSize = 1)
-    
-    neural_net.trained = True
+    neural_net = create_and_prep_net(input_width = WINDOW_SIZE[0],
+                                     input_height = WINDOW_SIZE[1],
+                                     input_depth = N_CHANNELS,
+                                     target_label_list = [UP,DOWN,NONE])
     
     # --- create policy gradient wrapper
     # create epsiode generator function
@@ -125,7 +68,7 @@ def main():
         # save checkpoint model ~ AI
         neural_net = policy_gradient_pong.ffnetwork
         neural_net_name = 'pong_pilot_' + str((training_chapter+1) * TRAINING_PAGES + pages_trained) + '_episodes'
-        neural_net.save(save_dir = PONG_MODEL_DIR, model_name = neural_net_name)
+        #neural_net.save(save_dir = PONG_MODEL_DIR, model_name = neural_net_name)
     
 if __name__ == "__main__":
     pg_pong = main()
--- lib/util.py
+++ lib/util.py
@@ -6,7 +6,90 @@ Created on Tue Apr  9 19:40:52 2019
 """
 
 import pickle
-import os
+import numpy as np
+import os,sys,inspect
+
+# custom imports - sys path shenanigans needed
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # working dir
+parentdir = os.path.dirname(currentdir) # lib dir of current repo: contains the game classes
+deep_learning_dir = os.path.join(os.path.dirname(parentdir),"deep_learning_library") # dir in dl repo: contains dl classes
+
+# diy deep learning lib from other repo
+sys.path.append(deep_learning_dir)
+from diy_deep_learning_library import FFNetwork
+
+def create_and_prep_net(input_width,
+                        input_height,
+                        input_depth,
+                        target_label_list):
+    '''Util function that builds and preps a convolutional pilot net for training.'''
+    # --- build conv net
+    # build network
+    neural_net = FFNetwork(2)
+    
+    kernelSize1 = 10
+    channels1 = 2
+    stride1 = 5
+    padding1 = 'valid'
+    
+    poolingSize2 = 5
+    stride2 = 2
+    padding2 = 'valid'
+    
+    kernelSize3 = 5
+    channels3 = 4
+    stride3 = 1
+    padding3 = 'valid'
+    
+    poolingSize4 = 2
+    stride4 = 2
+    padding4 = 'valid'
+    
+    n4 = 30
+    
+    n_output = 3
+    
+    neural_net.addConvLayer(kernelHeight=kernelSize1,
+                       kernelWidth=kernelSize1,
+                       channels=channels1,
+                       stride=stride1,
+                       padding=padding1,
+                       activation='tanh')
+    neural_net.addPoolingLayer(poolingHeight=poolingSize2,
+                           poolingWidth=poolingSize2,
+                           stride=stride2,
+                           padding=padding2,
+                           poolingType='max')
+    neural_net.addConvLayer(kernelHeight=kernelSize3,
+                           kernelWidth=kernelSize3,
+                           channels=channels3,
+                           stride=stride3,
+                           padding=padding3,
+                           activation='tanh')
+    neural_net.addPoolingLayer(poolingHeight=poolingSize4,
+                           poolingWidth=poolingSize4,
+                           stride=stride4,
+                           padding=padding4,
+                           poolingType='max')
+    
+    neural_net.addFlattenConvLayer()
+    
+    neural_net.addFCLayer(n4,activation='tanh')
+    
+    neural_net.addFCLayer(n_output,activation='softmax')
+    
+    neural_net.fixateNetwork(np.zeros((10,input_depth,input_width,input_height)))
+    
+    print(neural_net)
+    
+    # prep network for inference without training it
+    neural_net.oneHotY(np.array(target_label_list))
+
+    neural_net.initialize_layer_optimizers('sgd',eta = 0.001,gamma = 0.99,epsilon = 0.00000001,lamda = 0,batchSize = 1)
+    
+    neural_net.trained = True
+    
+    return neural_net
 
 def load_oldest_model(game,model_dir):
     '''Helper function that loads the most trained walker/pong model from specified model
@@ -17,7 +100,23 @@ def load_oldest_model(game,model_dir):
     assert os.path.isdir(model_dir)
     
     # get oldest model for given game/experiment
-    os.listdir(model_dir)
+    game_models = [model_name for model_name in os.listdir(model_dir) if game in model_name]
     
+    if len(game_models) != 0:
+        # get path to most trained model
+        episodes_trained = list(map(lambda model_name: model_name.split('_')[2],game_models))
+        pos = np.argmax(episodes_trained)
+        game_model_name, episodes_trained = game_models[pos], episodes_trained[pos]
+        game_model_path = os.path.join(model_dir,game_model_name)
+        
+        # load most trained model
+        with open(game_model_path,'rb') as game_model_file:
+            game_model = pickle.load(game_model_file)
+    else:
+        game_model, episodes_trained = None, 0
+        
+    return game_model, episodes_trained
+        
+        
     
     
